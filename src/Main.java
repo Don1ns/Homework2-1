7710821eb01c9de20f7a5ea8d31f21335b90d513
Рияз <riaz.karimullin@yandex.ru>
2022-11-29 12:57:16 +0300

    Урок 2. Домашннее задание 1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1.
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        //Задача 2.
+        dog += 4;
+        System.out.println(dog);
+        cat += 4;
+        System.out.println(cat);
+        paper += 4;
+        System.out.println(paper);
+        //Задача 3.
+        dog -= 3.5;
+        System.out.println(dog);
+        cat -= 1.6;
+        System.out.println(cat);
+        paper -= 7639;
+        System.out.println(paper);
+    }
+}
